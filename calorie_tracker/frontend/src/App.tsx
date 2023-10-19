05434aa34a87bb2e8af35fe9b983ed2538affba7
Pedro Souza <pedrohns97@hotmail.com>
2023-10-19 12:44:07 -0300

    Criação do react node 18.18.0

---
+++ calorie_tracker/frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='avatar'>
+        <div className='w-32 rounded oi'>
+          <img src="src/assets/male_user.png" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
